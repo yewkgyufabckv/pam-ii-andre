2b4149b4d76ca5604b33ebb50e6cf9305e61b61a
Andre Mateus <andredemouramateus@gmail.com>
2024-04-10 08:33:41 -0300

    Aula 03 sobre arrays e objeto

---
+++ aula03/variaveiselaso.js
@@ -0,0 +1,25 @@
+let n = 2
+let t = "oi"
+
+console.log(typeof n)
+console.log(typeof t)
+
+let a = [0,1,1,2,3,5,8,13,21]
+
+console.log(typeof a)
+
+let o = {nome: "André", idade: "17"} // JSON
+//Javascript object notation
+
+console.log(typeof o)
+
+console.log(a)
+console.log(a[6])
+
+a.forEach((el) => 
+    console.log(`Elemento: ${el}`)
+
+    )
+
+    console.log(o)
+    console.log(o.nome, "tem", o.idade)
